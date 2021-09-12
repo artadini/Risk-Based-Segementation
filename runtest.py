2575929a82fb6310d5dd378b4e2a9c72d35d7a5d
Nikolas Artadi <84403080+artadini@users.noreply.github.com>
2021-09-12 13:56:45 +0200

    new

---
+++ runtest.py
@@ -0,0 +1,5 @@
+from groupc_dct_risk import datareader_doc
+
+print(help(datareader_doc))
+
+print(datareader_doc())
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='groupc_dct_risk',
+      version='0.2',  # Development release
+      description='
+Group Project - Risk Based Segmentation
+This is document contains a description of the task and also a starter code. Implement your exercise by changing only this Jupyter Notebook and the class inside RiskDataFrame.py, deliver both files.
+
+Introduction
+Customer segmentation involves categorizing the portfolio by industry, location, revenue, account size, and number of employees and many other variables to reveal where risk and opportunity live within the portfolio. Those patterns can then provide key measurable data points for more predictive credit risk management. Taking a portfolio approach to risk management gives credit professionals a better fix on the accounts, in order to develop strategies for better serving segments that present the best opportunities. Not only that, you can work to maximize performance in all customer segments, even seemingly risky segments.
+
+Customer segmentation analysis can lead to several tangible improvements in credit risk management: stronger credit policies, and improved internal communication and cooperation across teams.
+
+Task scope
+Your group is working in the retail risk modeling team and you are asked to build a class to perform risk-based segmentation and test it for car loans’ customers based on given historical data of customer behavior. The class must perform the segmentation from a risk management perspective',
+      url='https://github.com/artadini/Risk-Based-Segementation',
+      author='Group C',
+      author_email='artadini@student.ie.edu',
+      license='MIT',
+          packages=['groupc_dct_risk'],
+      zip_safe=False)
